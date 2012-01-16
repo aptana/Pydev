6f7463bdd723a38ba6edab921ff6dc0251ac0d2b
Haw-Bin Chai <hbchai@gmail.com>
2012-01-16 14:21:35 -0500

    Initial pass at refactoring ParserScheduler and making it thread-safe.

--- plugins/org.python.pydev.parser/src/org/python/pydev/parser/ParserScheduler.java
+++ plugins/org.python.pydev.parser/src/org/python/pydev/parser/ParserScheduler.java
@@ -11,163 +11,176 @@
  */
 package org.python.pydev.parser;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Schedules Python parsing events. Each ParserScheduler instance has a timer
+ * thread on which all parsing is performed.
+ * 
+ * The public methods of this class are thread-safe.
+ */
 public class ParserScheduler {
-
-    /**
-     * used to do parsings in a thread - is null when not doing parsings
-     */
-    public volatile ParsingThread parsingThread; 
+    // The scheduler thread can perform the following transitions:
+    //     WAITING => PARSE_SCHEDULED
+    //     PARSING => PARSING_WITH_RESCHEDULE
+    //
+    // The parsing task timer thread can perform the following transitions:
+    //     WAITING => PARSING
+    //     PARSE_SCHEDULED => PARSING
+    //     PARSING => WAITING
+    //     PARSING_WITH_RESCHEDULE => PARSE_SCHEDULED
+    private enum State {
+        WAITING,                  // Waiting for a parse directive
+        PARSE_SCHEDULED,          // A parse has been scheduled for the future
+        PARSING,                  // Currently in a parse
+        PARSING_WITH_RESCHEDULE,  // In a parse, with auto-reschedule
+    }
     
-    /**
-     * indicates that currently nothing is happening
-     */
-    public static final int STATE_WAITING = 0; 
-    
-    /**
-     * indicates whether some parse later has been requested
-     */
-    public static final int STATE_PARSE_LATER = 1; 
-
-    /**
-     * indicates if a thread is currently waiting for an elapse cycle to end
-     */
-    public static final int STATE_WAITING_FOR_ELAPSE = 2;
-    
-    /**
-     * indicates if a thread is currently doing a parse action
-     */
-    public static final int STATE_DOING_PARSE = 3;
-    
-    /**
-     * initially we're waiting
-     */
-    int state = STATE_WAITING;
-    
-    /**
-     * this is the exact time a parse later was requested
-     */
-    private long timeParseLaterRequested = 0;
-    
-    /**
-     * this is the exact time the last parse was requested
-     */
-    private long timeLastParse = 0;
-
     private PyParser parser;
+    private Timer timer;
+    private State state;
+    private long scheduleInterval;
 
+    /**
+     * @param parser
+     *            The main Python parser for this scheduler. Must not be null.
+     */
     public ParserScheduler(PyParser parser) {
         super();
+        assert parser != null;
         this.parser = parser;
+        this.timer = new Timer(true);
+        this.state = State.WAITING;
+        this.scheduleInterval = PyParserManager.getPyParserManager(null)
+                .getElapseMillisBeforeAnalysis();
     }
-
-
+    
+    /**
+     * The same as parseNow(false) .
+     */
     public void parseNow() {
         parseNow(false);
     }
     
     /**
-     * The arguments passed in argsToReparse will be passed to the reparseDocument, and then on to fireParserChanged / fireParserError
+     * Initiate a parse. If no parse is currently happening, and none is
+     * scheduled, a parse is performed immediately.
      * 
-     * @return false if we asked a forced reparse and it will not be scheduled because a reparse is already in action.
+     * @param force
+     *            If true, and a parse is currently happening, simply returns
+     *            false. Otherwise, begins a parse immediately, even if a parse
+     *            is already scheduled. If false, and a parse is currently
+     *            happening, schedules another parse after it is done.
+     *            Otherwise, if a parse is scheduled, does nothing (i.e. waits
+     *            for that scheduled parse).
+     * @param argsToReparse
+     *            The arguments passed to the PyParser's reparseDocument().
+     * @return false if we asked a forced parse and it will not be scheduled
+     *         because a parse is already in action.
      */
-    public boolean parseNow(boolean force, Object ... argsToReparse) {
-        if(!force){
-            if(state != STATE_WAITING_FOR_ELAPSE && state != STATE_DOING_PARSE){
-                //waiting or parse later
-                state = STATE_WAITING_FOR_ELAPSE; // the parser will reset it later
-                timeLastParse = System.currentTimeMillis();
-                checkCreateAndStartParsingThread();
-            }else{
-                //another request... we keep waiting until the user stops adding requests
-                boolean created = checkCreateAndStartParsingThread();
-                if(!created){
-                    parsingThread.okToGo = false;
-                }
+    public synchronized boolean parseNow(boolean force, Object ... argsToReparse) {
+        // This method does not modify state, although calls to scheduleParse()
+        // may do so.
+        if (state == State.WAITING) {
+            scheduleParse(0, argsToReparse);
+        } else if (!force) {
+            if (state == State.PARSING) {
+                scheduleParse(scheduleInterval, argsToReparse);
+            } else {
+                // PARSE_SCHEDULED or PARSING_WITH_RESCHEDULE; just wait for the
+                // next scheduled event.
             }
-        }else{
-            ParsingThread parserThreadLocal = parsingThread; //if it dies suddenly, we don't want to get it as null...
-            if(state == ParserScheduler.STATE_DOING_PARSE){
-                //a parse is already in action
+        } else {
+            if (state == State.PARSING
+                    || state == State.PARSING_WITH_RESCHEDULE) {
                 return false;
-            }else{
-                if(parserThreadLocal == null){
-                    parserThreadLocal = new ParsingThread(this, argsToReparse);
-                    parsingThread = parserThreadLocal;
-                    parserThreadLocal.force = true;
-                    parserThreadLocal.setPriority(Thread.MIN_PRIORITY); //parsing is low priority
-                    parserThreadLocal.start();
-                }else{
-                    //force it to run
-                    parserThreadLocal.force = true;
-                    parserThreadLocal.interrupt();
-                }
+            } else {
+                // PARSE_SCHEDULED; kick of an "out of band" parse immediately.
+                // The previously scheduled parse becomes a "dangling" parse,
+                // which nevertheless should be well-behaved.
+                scheduleParse(0, argsToReparse);
             }
         }
         return true;
     }
-
-
+    
+    public synchronized void parseLater() {
+        if (state != State.PARSE_SCHEDULED && state != State.PARSING_WITH_RESCHEDULE) {
+            scheduleParse(scheduleInterval);
+        }
+    }
+    
+    private synchronized void scheduleParse(long delay, Object... args) {
+        // State transitions:
+        //     WAITING => PARSE_SCHEDULED
+        //     PARSING => PARSING_WITH_RESCHEDULE
+        if (state == State.WAITING) {
+            state = State.PARSE_SCHEDULED;
+        } else if (state == State.PARSING){
+            state = State.PARSING_WITH_RESCHEDULE;
+        }
+        
+        // A shared PyParser instance is passed into the task's constructor.
+        // This should be ok, because the tasks are serialized on the Timer
+        // thread.
+        this.timer.schedule(new ParseTask(this, this.parser, args), delay);
+    }
+    
     /**
-     * @return whether we really created the thread (returns false if the thread already exists)
+     * To be called back by the parsing task that wants to start parsing. The
+     * return value indicates whether the parse should proceed.
      */
-    private boolean checkCreateAndStartParsingThread() {
-        ParsingThread p = parsingThread;
-        if(p == null){
-            p = new ParsingThread(this);
-            p.setPriority(Thread.MIN_PRIORITY); //parsing is low priority
-            p.start();
-            parsingThread = p;
+    private synchronized boolean notifyParseIntent() {
+        // State transitions:
+        //     WAITING => PARSING
+        //     PARSE_SCHEDULED => PARSING
+        if (state == State.WAITING || state == State.PARSE_SCHEDULED) {
+            state = State.PARSING;
             return true;
         }
         return false;
     }
     
-    public void parseLater() {
-        if(state != STATE_WAITING_FOR_ELAPSE && state != STATE_PARSE_LATER){
-            state = STATE_PARSE_LATER;
-            //ok, the time for this request is:
-            timeParseLaterRequested = System.currentTimeMillis();
-            Thread thread = new Thread(){
-                @Override
-                public void run() {
-                    try {
-                        sleep(PyParserManager.getPyParserManager(null).getElapseMillisBeforeAnalysis());
-                    } catch (Exception e) {
-                        //that's ok
-                    }
-                    //ok, no parse happened while we were sleeping
-                    if( state == STATE_PARSE_LATER && timeLastParse < timeParseLaterRequested){
-                        parseNow();
-                    }
-                }
-            };
-            thread.setName("ParserScheduler");
-            thread.start();
+    /**
+     * To be called back by the parsing task when a parse has been completed.
+     */
+    private synchronized void notifyParseComplete() {
+        // State transitions:
+        //     PARSING => WAITING
+        //     PARSING_WITH_RESCHEDULE => PARSE_SCHEDULED
+        if (state == State.PARSING) {
+            state = State.WAITING;
+        } else if (state == State.PARSING_WITH_RESCHEDULE) {
+            state = State.PARSE_SCHEDULED;
+        }
+    }
+    
+    /**
+     * A parsing task that notifies the scheduler of parsing events.
+     */
+    private static class ParseTask extends TimerTask {
+        private ParserScheduler scheduler;
+        private PyParser parser;
+        private Object[] args;
+
+        public ParseTask(ParserScheduler scheduler, PyParser parser,
+                Object[] args) {
+            this.scheduler = scheduler;
+            this.parser = parser;
+            this.args = args;
         }
         
-    }
-
-
-    /**
-     * this should call back to the parser itself for doing a parse
-     * 
-     * The argsToReparse will be passed to the IParserObserver2
-     */
-    public void reparseDocument(Object ... argsToReparse) {
-        PyParser p = parser;
-        if(p != null){
-            p.reparseDocument(argsToReparse);
+        public void run() {
+            if (this.scheduler.notifyParseIntent()) {
+                this.parser.reparseDocument(args);
+                this.scheduler.notifyParseComplete();
+            }
         }
     }
-
-
+    
     public void dispose() {
-        ParsingThread p = this.parsingThread;
-        if(p != null){
-            p.dispose();
-        }
         this.parser = null;
+        this.timer.cancel();
     }
-
-
 }
